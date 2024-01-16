dd664b5b6cab746f47754a580e3b2dc0cd18af70
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-01-17 00:37:11 +0300

    topgames

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gameapp_flutter/data.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +9,59 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  var _deviceHeight;
+  var _deviceWidth;
+
+  var _selectedGame;
+
+  @override
+  void initState(){
+    super.initState();
+    _selectedGame = 0;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          _topGamesWidget(),
+        ],
+      ),
+    );
+  }
+
+  Widget _topGamesWidget(){
+    return SizedBox(
+      height: _deviceHeight * 0.5,
+      width: _deviceWidth,
+      child: PageView(
+        onPageChanged: (_index){
+          setState(() {
+            _selectedGame = _index;
+          });
+        },
+        scrollDirection: Axis.horizontal,
+        children: topgames.map((_game) {
+          return Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit:BoxFit.cover,
+                image:NetworkImage(_game.coverImage.url),
+              ),
+
+            ),
+
+          );
+        }).toList(),
+      ),
+
+
+    );
+
+
   }
 }
